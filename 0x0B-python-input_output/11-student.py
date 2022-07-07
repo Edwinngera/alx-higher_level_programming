ed6a80962ea7a915d6b116917bc76600f4f5a610
Edwinngera <edwinngera@gmail.com>
2022-07-07 19:40:15 +0000

    task11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 11-student.py
+# 13-student.py
 # Brennan D Baraban <375@holbertonschool.com>
 """Defines a class Student."""
 
@@ -19,6 +19,25 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get a dictionary representation of the Student."""
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
