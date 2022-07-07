a1361361e54b9ec48db12cd2c02fff16d8f5ff2d
Edwinngera <edwinngera@gmail.com>
2022-07-07 19:48:16 +0000

    task3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# 6-from_json_string.py
+# 5-to_json_string.py
 # Brennan D Baraban <375@holbertonschool.com>
-"""Defines a JSON-to-object function."""
+"""Defines a string-to-JSON function."""
 import json
 
 
-def from_json_string(my_str):
-    """Return the Python object representation of a JSON string."""
-    return json.loads(my_str)
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
